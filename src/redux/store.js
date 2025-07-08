99bca8a0743b349bcd83051a687939350f9c908d
“Kevincode6” <maloukevin6@gmail.com>
2025-07-08 08:06:24 +0200

    first full commit

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import ticketReducer from '/ticketSlice';
+import adminReducer from './adminSlice';
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        tickets: ticketReducer,
+        admin: adminReducer,
+    },
+});
